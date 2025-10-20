a25ed620fa8de88dfe571b79c881e6d9ceb1b90c
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 14:42:39 +0000

    single digits

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -6,26 +6,25 @@
  * main - Entry Point
  * Return: Always 0
  */
-
 int main(void)
 {
-int firstNumbers, secondNumbers;
-for (firstNumbers = 0; firstNumbers < 10; firstNumbers++)
-{
-    for (secondNumbers = firstNumbers + 1; secondNumbers < 10; secondNumbers++)
-    {
-        putchar(firstNumbers + '0');
-        putchar(',');
-        putchar(' ');
-        putchar(secondNumbers + '0');
-        if (firstNumbers != 8 || secondNumbers != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-}
-putchar('\n');
-return (0);
+	int i, j;
 
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
