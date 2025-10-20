828bf366135aa52ad73a5e0680b4136a833e36a5
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 13:49:13 +0000

    reverse letters

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char myChar;
+
+for (myChar = 'z'; myChar >= 'a'; myChar--)
+{
+putchar(myChar);
+}
+putchar('\n');
+return (0);
+}
