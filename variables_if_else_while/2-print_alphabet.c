96f406fac33c69d96ecd0e0e2c990b6cb394a1cd
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 12:17:37 +0000

    updates

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
 int main(void)
 {
- char myChar;
+char myChar;
 
- for (myChar = 'a'; myChar <= 'z'; myChar++) 
+for (myChar = 'a'; myChar <= 'z'; myChar++) 
 {
- putchar(myChar);
+putchar(myChar);
 }
 putchar('\n'); 
-    return(0);
+return(0);
 }
