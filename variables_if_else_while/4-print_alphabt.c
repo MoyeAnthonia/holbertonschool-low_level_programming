36105e59e554bbfd2f742bdc42462a29e7173c83
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 12:56:23 +0000

    omit letters

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char myChar;
+
+for (myChar = 'a'; myChar <= 'z'; myChar++ )
+{
+if(myChar != 'q' && myChar != 'e')
+{
+putchar(myChar);
+}
+}
+putchar('\n');
+return (0);
+}
