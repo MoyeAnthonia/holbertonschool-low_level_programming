ff14453741308b8345d8ab9e23471b3e07fdeead
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 12:14:53 +0000

    Print lowercase alphabets

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+ char myChar;
+
+ for (myChar = 'a'; myChar <= 'z'; myChar++) 
+{
+ putchar(myChar);
+}
+putchar('\n'); 
+    return(0);
+}
