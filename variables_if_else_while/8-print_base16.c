55b3faa378368ec5846c413514526cc020acc677
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-20 14:10:42 +0000

    print hexdecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int myNumbers;
+
+for (myNumbers = 0; myNumbers < 16; myNumbers++)
+{
+if (myNumbers < 10)
+{
+putchar(myNumbers + '0');
+}
+else{
+putchar(myNumbers - 10 + 'a');
+}
+}
+putchar('\n');
+return (0);
+}
