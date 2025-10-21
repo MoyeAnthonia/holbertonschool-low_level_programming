291800219eb40501bf047d8a8728c7d92a01ccf3
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-21 11:56:22 +0000

    fix documentation

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ *  print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ 
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
