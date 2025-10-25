3da0ad87e15585be304f43e08a10c3eb3ddcf0bd
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 02:30:56 +0000

    n

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -15,11 +15,11 @@ int count;
 
 for (count = 0; count <= 9; count++)
 {
-    if (count == 2 || count == 4)
-    {
-        continue;
-    }
+if (count == 2 || count == 4)
+{
+continue;
+}
 _putchar(count + '0');
 }
-_putchar('\n'); 
+_putchar('\n');
 }
