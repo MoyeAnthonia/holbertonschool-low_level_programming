84180929916c277b675d9310ef9ea4cf785a363c
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 01:57:52 +0000

    fix betty

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * 2-mul -  function that multiplies two integers.
+ * mul -  function that multiplies two integers.
  * @a: The character to check.
  * @b: The character to check.
  *
-* Description: This function that multiplies two integers.
+* Description: This function that multiplies two integers
+* passed as an argument corresponds two integers.
 *
-* Return: a or b 
+* Return: a or b
 */
 int mul(int a, int b)
 {
