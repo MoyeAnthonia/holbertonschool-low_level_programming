a4aacd006b7ef199a038a509a322b55d54c034f7
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 13:27:48 +0000

    print n umber

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints 10 times the numbers, from 0 to 14.
+ * followed by a new line.
+ *
+* Description: function that prints 10 times the numbers, from 0 to 14.
+* Do not print 2 and 4
+*
+* Return: 1  that prints the numbers, from 0 to 9.
+*/
+void more_numbers(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j >= 10)
+_putchar('1');
+_putchar(j % 10 + '0');
+}
+_putchar('\n');
+}
+}
+ 
