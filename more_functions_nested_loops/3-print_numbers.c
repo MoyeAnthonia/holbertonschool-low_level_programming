c2b6cdcf32f4c5d9be953e761ae5f13dbbc46111
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 02:20:53 +0000

    .

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -13,9 +13,9 @@ void print_numbers(void)
 {
 int count;
 
-for (count = 0; count < 10; count++)
+for (count = 0; count <= 9; count++)
 {
-_putchar(count);
+_putchar(count + '0');
 }
 _putchar('\n');
 }
