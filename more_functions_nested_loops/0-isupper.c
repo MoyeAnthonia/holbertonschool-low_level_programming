b47e429a77a7ff9d8487c5fce574b707a4473810
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 01:19:16 +0000

    fix betty documentation

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
 /**
- * function that checks for uppercase character
- */
+ * _isupper - function that checks for uppercase character.
+ * @c: The character to check.
+
+* Description: This function checks whether the character code
+* passed as an argument corresponds to an uppercase letter (A–Z).
+*
+* Return: 1 if c is an uppercase letter, 0 otherwise.
+*/
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
