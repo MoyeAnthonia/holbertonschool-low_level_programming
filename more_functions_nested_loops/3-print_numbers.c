209c4228577bf496133a45625067529cda8cac1b
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-25 02:09:21 +0000

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ * @void: The character to check.
+ *
+* Description: This function that prints the numbers, from 0 to 9
+* function that prints the numbers, from 0 to 9.
+*
+* Return: 1  that prints the numbers, from 0 to 9.
+*/
+void print_numbers(void)
+{
+int count;
+
+for (count = 0; count < 9; count++)
+{
+_putchar(count);
+}
+_putchar('\n');
+}
