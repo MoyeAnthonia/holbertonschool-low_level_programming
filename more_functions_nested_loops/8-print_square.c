5030410140a801a20be34e6444a402668b629632
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-26 15:01:25 +0000

    fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@
 */
 void print_square(int size)
 {
-int i;
+int i, j;
 if (size <= 0)
 {
 _putchar('\n');
@@ -19,8 +19,11 @@ else
 {
 for (i = 0; i < size; i++)
 {
+for (j = 0; j < size; j++)
+{
 _putchar('#');
 }
 _putchar('\n');
+}
 }
 }
