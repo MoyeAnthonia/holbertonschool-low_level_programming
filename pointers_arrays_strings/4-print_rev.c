753dfa0497ce0d61a44b964a18db1c7f74e833e9
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-27 13:36:01 +0000

    fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,8 +11,10 @@
 */
 void print_rev(char *s)
 {
-int length = strlen(s);
-int i = 0;
+int length = 0;
+int i;
+while (s[length] != '\0')
+length++;
 for (i = length - 1; i >= 0; i--)
 _putchar(s[i]);
 _putchar('\n');
