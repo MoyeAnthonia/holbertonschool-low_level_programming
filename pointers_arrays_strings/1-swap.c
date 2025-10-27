7970561fc1be328728dd02ac9dccf3bcaeb79596
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-27 02:30:11 +0000

    swap pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap -  function that swaps the values of two integers.
+  * @n: The number of the parameter
+ *
+* Description: If n is 0 or less, prints only a newline.
+*
+*
+*/
+void swap_int(int *a, int *b)
+{
+int temp = *ptr1;
+*ptr1 = *ptr2;
+*ptr2 = temp;
+}
