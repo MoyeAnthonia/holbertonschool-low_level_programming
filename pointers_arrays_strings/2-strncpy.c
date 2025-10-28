46e6e0f10a8d486eba519fe9bfe8dda42f1f7361
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-28 11:04:13 +0000

    copy strings

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,19 @@
 #include "main.h"
 
 /**
- * leet - encodes a string into 1337
- * @s: string to encode
+ * _strncpy - copies a string
+ * @dest: destination buffer
+ * @src: source string
+ * @n: number of bytes to copy
  *
- * Return: pointer to the encoded string
+ * Return: pointer to the resulting string dest
  */
-char *leet(char *s)
+char *_strncpy(char *dest, char *src, int n)
 {
-int i, j;
-char letters[] = "aAeEoOtTlL";
-char numbers[] = "4433007711";
-for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; letters[j] != '\0'; j++)
-{
-if (s[i] == letters[j])
-{
-s[i] = numbers[j];
-break;
-}
-}
-}
-return (s);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for (; i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
