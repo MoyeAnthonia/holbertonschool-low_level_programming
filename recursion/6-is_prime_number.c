b5894d987eed3fe98026d1ab314523b610928b6d
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-30 14:17:30 +0000

    prime number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+ * _is_prime - helper function to check divisibility recursively
+ * @n: the number to check
+ * @i: current divisor
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int _is_prime(int n, int i)
+{
+if (i * i > n)
+return (1);
+if (n % i == 0)
+return (0);
+return (_is_prime(n, i + 1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: the number to check
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (_is_prime(n, 2));
+}
