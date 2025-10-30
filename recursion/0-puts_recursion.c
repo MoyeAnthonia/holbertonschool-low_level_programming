cfddecb6a0b4171dd41434465c192023f9fd950b
MoyeAnthonia <moye.anthonia@gmail.com>
2025-10-30 03:20:06 +0000

    recursion set up

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * 0-puts_recursion - function that prints a string, followed by a new line.
+ * @str: string to print.
+ *
+ * Description: function that prints a string.
+ * followed by a new line.
+ */
+void _puts_recursion(char *s)
+{
+
+}
