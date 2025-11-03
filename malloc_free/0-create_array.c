174cfe2ae2e3743185c5f885f66d3a77e0ad9fb6
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-03 13:00:01 +0000

    setup malloc directory

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _create_array - copies memory area
+ * @size: destination memory area
+ * @c: source memory area
+ *
+ * Return: 0 always
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+}
