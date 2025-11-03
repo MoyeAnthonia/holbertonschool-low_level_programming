0125872be37ea4499288e07618c672d3dc7bb2af
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-03 13:39:30 +0000

    add files

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory
+ * @str: the string to initalizie.
+ *
+ * Return: pointer to the array, or NULL if it fails or size = 0
+ */
+char *_strdup(char *str)
+{
+if (str == NULL)
+return (NULL);
+
+}
