41779ab4b751593c6537c7036de8dbae01693f9c
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-08 14:36:51 +0000

    fix

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,13 +4,25 @@
 
 /**
  * array_range - function that concatenates two strings.
- * @s1: the first array.
- * @s2: the second array.
- * @n: the size of the array
+ * @min: the first array.
+ * @max: the second array. 
  *
  * Return: 0
  */
 int *array_range(int min, int max)
 {
+int *arr;
+int i, size;
 
+if (min > max)
+return (NULL);
+size = max - min + 1;
+
+arr = malloc(size *sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+arr[i] = min + i;
+
+return (arr);
 }
