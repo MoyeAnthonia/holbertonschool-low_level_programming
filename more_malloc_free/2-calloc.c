4a93605e04c4e4695cd255134fd8fffb8e3bcd3d
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-08 14:20:26 +0000

    add fix

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * calloc - function that concatenates two strings.
+ * @s1: the first array.
+ * @s2: the second array.
+ * @n: the size of the array
+ *
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    
+}
---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - function that concatenates two strings.
+ * @s1: the first array.
+ * @s2: the second array.
+ * @n: the size of the array
+ *
+ * Return: 0
+ */
+int *array_range(int min, int max)
+{
+
+}
