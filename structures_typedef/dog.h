2456507ae0b15e8c9152c178fc33230d40a4b149
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-09 12:09:12 +0000

    add dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Represents a dog with basic attributes
+ * @name: The name of the dog
+ * @age: The age of the dog
+*  @owner: The owner facilities the services.
+ *
+ * This structure stores information about a dog.
+ */
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
