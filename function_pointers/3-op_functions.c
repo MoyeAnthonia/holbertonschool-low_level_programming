54192099e3d8caaa55e3f973218dbc0e8bbd8bfc
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-15 10:09:38 +0000

    functions for program

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,11 +1,13 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
 /**
 * This file contains the functions used for the program
 * op_add - Function that returns the sum of a and b
 * op_sub - Function that returns the difference of a and b
-* op_mul - Function that returns the sum of a and b
-* op_add - Function that returns the sum of a and b
-* op_add - Function that returns the sum of a and b
+* op_mul - Function that returns the products of a and b
+* op_div - Function that returns the division of a and b
+* op_mod - Function that returns modular of the divison of a and b
 * @a: first value
 * @b: second value
 *
@@ -19,6 +21,15 @@ int op_sub(int a, int b)
 {
 return a - b;
 }
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
+int op_mul(int a, int b)
+{
+return a * b;
+}
+int op_div(int a, int b)
+{
+return a / b;
+}
+int op_mod(int a, int b)
+{
+return a % b;
+}
