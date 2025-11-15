c05dfcf47d097c4a8f8fd9a4732b997186b11ebb
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-15 06:13:40 +0000

    function to print out names

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
 
+/**
+ * _print_name - function that prints a name.
+ * @name: pointer to name
+ *
+ * Return: 0 always
+ */
 void print_name(char *name, void (*f)(char *))
 {
-
+if (f == NULL || name == NULL)
+return;
+f(name);
 }
