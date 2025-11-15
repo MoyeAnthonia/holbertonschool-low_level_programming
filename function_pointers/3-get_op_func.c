c2e2c4bdebc95abca714149509e089a04dae2c9b
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-15 10:03:11 +0000

    writing code

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,4 @@
+/**
+function that selects the correct function to perform the operation asked by the user.
+*/
+int (*get_op_func(char *s))(int, int);
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+/**
+* This file contains the functions used for the program
+* op_add - Function that returns the sum of a and b
+* op_sub - Function that returns the difference of a and b
+* op_mul - Function that returns the sum of a and b
+* op_add - Function that returns the sum of a and b
+* op_add - Function that returns the sum of a and b
+* @a: first value
+* @b: second value
+*
+* return results
+*/
+int op_add(int a, int b)
+{
+return a + b;
+}
+int op_sub(int a, int b)
+{
+return a - b;
+}
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
