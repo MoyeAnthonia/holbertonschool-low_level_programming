ea0527f68a622cffc1825fd121874491f767c64c
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-16 12:46:21 +0000

    betty code fix

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,47 +7,44 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char *sep = "";
+va_list args;
+int i = 0;
+char *str;
+char *sep = "";
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':
-                printf("%s%c", sep, va_arg(args, int));
-                break;
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+break;
 
-            case 'i':
-                printf("%s%d", sep, va_arg(args, int));
-                break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+break;
 
-            case 'f':
-                printf("%s%f", sep, va_arg(args, double));
-                break;
+default:
+i++;
+continue;
+}
+sep = ", ";
+i++;
+}
 
-            case 's':
-                str = va_arg(args, char *);
-                if (!str)        /* IF #1 */
-                    str = "(nil)";
-                printf("%s%s", sep, str);
-                break;
+if (1)
+printf("\n");
 
-            default:
-                i++;
-                continue;
-        }
-
-        sep = ", ";
-        i++;
-    }
-
-    if (1)
-        printf("\n");
-
-    va_end(args);
+va_end(args);
 }
