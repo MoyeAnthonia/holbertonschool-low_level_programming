d338ee821dc4c1d6b9bf59013c9f98a70ff21a6b
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-29 14:33:28 +0000

    check read error

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -49,5 +49,14 @@ close(fd_to);
 exit(99);
 }
 }
+
+/** check read error*/
+if (r == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+close(fd_from);
+close(fd_to);
+exit(98);
+}
 return (0);
 }
