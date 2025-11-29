2c60c4653f39fc4b8ca7084d60f7a3a778c8ff98
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-29 14:59:02 +0000

    create files

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list -   function that prints all the elements of a list_t list.
+ * @h: the nodes
+ *
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+
+h = h->next;
+count++;
+}
+return (count);
+}
