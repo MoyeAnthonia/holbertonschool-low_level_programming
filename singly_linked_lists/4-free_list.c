d3e3bd95374d99a9c8477245c83b73edec646277
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-29 15:28:30 +0000

    free lists

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * free_list - function that frees list_t list.
+ * @head: the nodes
+ *
+ * Return: 0
+ */
 
 void free_list(list_t *head)
 {
+list_t *temp;
 
+while (head != NULL)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
