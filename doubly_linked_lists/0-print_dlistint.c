80e7bfa561c167393d144d18e03755a979220fef
MoyeAnthonia <moye.anthonia@gmail.com>
2025-11-30 11:34:47 +0000

    fix betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint-   function that prints all the elements of a dlistint_t list.
+ * print_dlistint-   function that prints all the elements
+ * of a dlistint_t list.
  * @h: the nodes
  *
  * Return: 0
