70fdc9a716314cb5b3b4a21cdba4903be55362da
MoyeAnthonia <moye.anthonia@gmail.com>
2025-12-07 09:51:27 +0000

    fix key pointer issue

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 unsigned long int index;
 hash_node_t *node;
 
-if (!ht || !key || *key)
+if (!ht || !key || !*key)
 return (NULL);
 
 index = key_index((const unsigned char *) key, ht->size);
@@ -26,6 +26,7 @@ while (node != NULL)
 {
 if (strcmp(node->key, key) == 0)
 return (node->value);
+
 node = node->next;
 }
 
