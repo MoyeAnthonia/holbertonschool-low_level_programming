9bfd30a4cae4a0e6611dc87c724862dddb4b8aa1
MoyeAnthonia <moye.anthonia@gmail.com>
2025-12-07 09:46:01 +0000

    retrives a value in hash

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - function that retrieves a value
+ * associated with a key.
+ * @key: is the key to hash
+ * @ht: hash table
+ *
+ * Return: 0
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int index;
+hash_node_t *node;
+
+if (!ht || !key || *key)
+return (NULL);
+
+index = key_index((const unsigned char *) key, ht->size);
+node = ht->array[index];
+
+while(node)
+{
+if (strcmp(node->key, key) == 0)
+return (node->value);
+node = node->next;
+}
+
+return (NULL);
+}
